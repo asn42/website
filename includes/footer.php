2d393b5587794a10cfc8f00021e1e4023b4906b1
root <dbourdon@student.42.fr>
2017-02-03 14:25:04 -0500

    commit

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 		      <!-- Site footer -->
 	      <footer class="footer">
-	        <p>© 2016 Company, Inc.</p>
+	        <p> <i class="fa fa-copyright fa-rotate-180"></i> Copyleft - 2017 ASN - <a href="https://github.com/asn42/website">Github</a></p>
 	      </footer>
 
 	    </div>
--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,8 @@
 <!doctype html>
-<html>
+<html lang="fr">
 	<head>
 		<meta doctype="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>[ASN] - Libre et sécurité à 42</title>
 		<link rel="stylesheet" href="includes/css/bootstrap-theme.min.css">
 		<link rel="stylesheet" href="includes/css/bootstrap.min.css">
@@ -10,45 +11,23 @@
 	</head>
 	<body>
 	<nav class="navbar navbar-default navbar-fixed-top">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" id="nav1" href="#"><span class="name_header">Association Sans Nom <br /><span class="text-muted">Libre et Sécurité à 42</span></span></a>
-    </div>
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li><a href="#">Hébergement Web<span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Configuration</a></li>
-        <li><a href="#">Création / Gestion</a></li>
-      </ul>
-      <ul class="nav navbar-nav navbar-right">
-        <a href="https://panel.hostack.fr" target="_blank" class="btn btn-success navbar-btn"><i class="fa fa-columns" aria-hidden="true"></i> &nbsp;Espace Client</a>
-      </ul>
-    </div>
-  </div>
-<!-- </nav>
-	 <nav class="navbar navbar-default">
 	  <div class="container-fluid">
 	    <div class="navbar-header">
-	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
 	        <span class="sr-only">Toggle navigation</span>
 	        <span class="icon-bar"></span>
 	        <span class="icon-bar"></span>
 	        <span class="icon-bar"></span>
 	      </button>
-	      <a class="navbar-brand" href="#"><span class="name_header">Association Sans Nom <br /><span class="text-muted">Libre et Sécurité à 42</span></span></a>
+	      <a class="navbar-brand" id="nav1" href="#"><span class="name_header">Association Sans Nom <br /><span class="text-muted">Libre et Sécurité à 42</span></span></a>
 	    </div>
 	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 	      <ul class="nav navbar-nav">
-	        <li><a href="#">Link <span class="sr-only">(current)</span></a></li>
-	        <li><a href="#">Link</a></li>
+	        <li><a href="#">Event<span class="sr-only">(current)</span></a></li>
+	        <li><a href="#">Staff</a></li>
+	        <li><a href="#">truc</a></li>
 	      </ul>
-	    </div><! /.navbar-collapse
-	  </div><! /.container-fluid
-	</nav> -->
+	    </div>
+	  </div>
+	  </nav>
 	<div class="container">
