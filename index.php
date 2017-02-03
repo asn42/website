546c3d017a7312a3fb80ff10504114be6cbb5db2
d-bourdon <dbourdon@student.42.fr>
2017-02-03 08:49:00 -0500

    fichiers remonter d' 1 + ajout de bootstrap

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<meta doctype="utf-8">
+<title>Association Sans Nom - Libre et sécurité à 42</title>
+<link rel="stylesheet" href="includes/css/style.css">
+</head>
+<body>
+<h1>Association Sans Nom</h1>
+<h2>Libre et sécurité à 42</h2>
+</body>
+</html>
